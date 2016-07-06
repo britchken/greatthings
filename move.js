677c3814f2abb3389042a940ed43029cbc0c1e6b
Brian Ritchken <bjr96@cornell.edu>
2016-07-06 14:07:18 -0400

    Did something?
    ouch

--- move.js
+++ move.js
@@ -1,11 +1,12 @@
-var xspeed;
-var yspeed;
+var xxspeed;
+var yyspeed;
 var h;
 var w;
+var xx;
+var yx;
+var tt;
 
-var theta;
-
-function makeNewPosition(x, y){
+function makeNewPosition(x, y, xspeed, yspeed, theta){
 
     if (x > w - 100 || x < 100 || y > h - 100 || y < 100) {
         theta = theta + .6;
@@ -19,38 +20,40 @@ function makeNewPosition(x, y){
     var nh = y + xspeed * Math.sin(theta);
     var nw = x + yspeed * Math.cos(theta);
 
-    return [nh,nw];    
+    return [nh,nw, theta];    
 }
 
 function done() {
     alert("Your jelly has been going for a while!");
 }
 
-function letsGo() {
-    $('.a').each(function(){
-        animateDivStart(this);
-    });
-}
 
-function animateDiv(that){
+function animateDiv(x, y, xspeed, yspeed, theta){
     var oldq = $('.a').offset();
-    var newq = makeNewPosition(oldq.left, oldq.top);
-    $(that).animate({rotate: theta/Math.PI*180 + 91}, 350);
+    var newq = makeNewPosition(oldq.left, oldq.top, theta);
+
+
+    $(that).animate({rotate: newq[2]/Math.PI*180 + 91}, 350);
     $(that).animate({ top: newq[0], left: newq[1]}, 800, "easeInOutBack", function(){
       animateDiv(that);        
     });
 
 };
 
-function animateDivStart(that) {
-
-    xspeed = 100;
-    yspeed = 100;
+function animateDivStart() {
     h = $(window).height() - 50;
     w = $(window).width() - 50;
-    totalJells = 0;
-    theta = 0;
-    animateDiv(that);
+    for (i = 0; i < 10; i = i + 1) {
+	xx[i] = Math.floor(Math.random()*w);
+	yy[i] = Math.floor(Math.random()*h);
+	theta[i] = 2*Math.random()*Math.PI;
+	yyspeed[i] = Math.floor(Math.random()*200);
+        xxspeed[i] = Math.floor(Math.random()*200);
+	}
+    
+    $('.a').each(function (idx, a)
+	{
+	$(a).animateDiv({left: xx[idx].x, top: ep[idx].y}, 800, "easeInOutBack", 
     
 }
 
