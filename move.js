aace7b25050c78833e0202e6c00701345281b2c9
Brian Ritchken <bjr96@cornell.edu>
2016-07-06 01:27:30 -0400

    done for tonight

--- move.js
+++ move.js
@@ -7,7 +7,7 @@ var theta;
 
 function makeNewPosition(x, y){
 
-    if (x > w - 70 || x < 70 || y > h - 70 || y < 70) {
+    if (x > w - 100 || x < 100 || y > h - 100 || y < 100) {
         theta = theta + .6;
     } else {
         if (Math.random() > .5) {
@@ -22,30 +22,38 @@ function makeNewPosition(x, y){
     return [nh,nw];    
 }
 
-/*
-function slowRotate(angle){
-    for (i = 0; i < 5; i++) {
-        $('.a').animate({rotate: i/4*angle}, 0);
-    }
+function done() {
+    alert("Your jelly has been going for a while!");
 }
-*/
-function animateDiv(){
 
+function letsGo() {
+    $('.a').each(function(){
+        animateDivStart(this);
+    });
+}
+
+function animateDiv(that){
     var oldq = $('.a').offset();
     var newq = makeNewPosition(oldq.left, oldq.top);
-    $('.a').animate({rotate: theta/Math.PI*180 + 90}, 250);
-    $('.a').animate({ top: newq[0], left: newq[1]}, 800, "easeInOutBack", function(){
-      animateDiv();        
+    $(that).animate({rotate: theta/Math.PI*180 + 91}, 350);
+    $(that).animate({ top: newq[0], left: newq[1]}, 800, "easeInOutBack", function(){
+      animateDiv(that);        
     });
 
 };
 
-function animateDivStart() {
+function animateDivStart(that) {
+
     xspeed = 100;
     yspeed = 100;
     h = $(window).height() - 50;
     w = $(window).width() - 50;
+    totalJells = 0;
     theta = 0;
-    //$( "p" ).append("<div class='a'></div>");
-    animateDiv();
+    animateDiv(that);
+    
 }
+
+
+
+
